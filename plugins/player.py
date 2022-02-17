bdae395dc2e7032639b34ace7808840cda12f16e
VENOMxCRAZY9 <97359676+VENOMxCRAZY9@users.noreply.github.com>
2022-02-18 04:04:06 +0530

    Update player.py

--- plugins/player.py
+++ plugins/player.py
@@ -284,7 +284,7 @@ async def play(_, message: Message):
                         InlineKeyboardButton("• Cʜᴀɴɴᴇʟ", url=f"https://t.me/V3n0m_0p"),
                       InlineKeyboardButton("• Group", url=f"https://t.me/v3nom_support"),
                   ],[
-                      InlineKeyboardButton("OWNER", url=f"https://t.me/VENOMxCRAZY")
+                      InlineKeyboardButton("𝐎 𝐖 𝐍 𝐄 𝐑", url=f"https://t.me/ZAAL_IM")
 
                 ]
             ]
